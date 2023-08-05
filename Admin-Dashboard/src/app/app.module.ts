77f622ee0145c831635965d5d3aa67bbd8900484
curtispandes <curtispandes@gmail.com>
2023-08-04 20:00:20 -0700

    init

--- Admin-Dashboard/src/app/app.module.ts
+++ Admin-Dashboard/src/app/app.module.ts
@@ -5,12 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
 import { ProductlistComponent } from './products/productlist/productlist.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
+import { ProductViewComponent } from './products/product-view/product-view.component';
+import { UsersComponent } from './users/users.component';
+import { OrdersComponent } from './orders/orders.component';
+import { PaymentComponent } from './payment/payment.component';
+import { ShipmentComponent } from './shipment/shipment.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AuthComponent,
-    ProductlistComponent
+    ProductlistComponent,
+    ProductListComponent,
+    ProductViewComponent,
+    UsersComponent,
+    OrdersComponent,
+    PaymentComponent,
+    ShipmentComponent
   ],
   imports: [
     BrowserModule,
